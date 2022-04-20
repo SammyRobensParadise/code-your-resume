eed957b1013019b271d986fc5728917723e7b395
Sammy Robens-Paradise <srobensparadise@gmail.com>
2022-04-20 17:05:41 -0400

    lint updating

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,116 +1,22 @@
 module.exports = {
   env: {
-    browser: true
+    browser: true,
+    es2021: true
   },
   extends: [
-    'airbnb-typescript',
-    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'prettier',
-    'next/core-web-vitals',
-    'next',
     'next/core-web-vitals'
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json'
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'prettier',
-    'import',
-    'testing-library'
-  ],
-  rules: {
-    /* NOTE: https://github.com/facebook/create-react-app/pull/8177 */
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        types: {
-          'React.FC': null
-        }
-      }
-    ],
-    '@typescript-eslint/naming-convention': [
-      'error',
-      {
-        selector: ['variable'],
-        format: ['camelCase', 'PascalCase', 'snake_case', 'UPPER_CASE']
-      }
-    ],
-    'import/order': [
-      'error',
-      {
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index'
-        ],
-        'newlines-between': 'always',
-        pathGroups: [
-          {
-            pattern: '~/**',
-            group: 'internal'
-          }
-        ]
-      }
-    ],
-    'no-param-reassign': ['error', { props: false }],
-    'prettier/prettier': 'error',
-    'react/jsx-props-no-spreading': 'off',
-    /* NOTE: https://github.com/yannickcr/eslint-plugin-react/issues/2396 */
-    'react/require-default-props': 'off',
-    'react/display-name': ['off'],
-    '@typescript-eslint/no-floating-promises': ['off'],
-    'jsx-a11y/anchor-is-valid': ['off']
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-        paths: './tsconfig.json'
-      }
+    ecmaFeatures: {
+      jsx: true
     },
-    react: {
-      pragma: 'React',
-      version: 'detect'
-    }
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  overrides: [
-    {
-      files: ['./cypress/**/*.{ts,tsx}'],
-      extends: [
-        'plugin:mocha/recommended',
-        'plugin:cypress/recommended',
-        'plugin:chai-friendly/recommended',
-        'plugin:testing-library/react'
-      ],
-      parserOptions: {
-        project: ['./cypress/tsconfig.json']
-      },
-
-      rules: {}
-    },
-    {
-      env: {
-        jest: true
-      },
-      files: ['./jest/**/*.{ts,tsx}'],
-      extends: [
-        'plugin:jest/recommended',
-        'plugin:jest/style',
-        'plugin:testing-library/react'
-      ],
-      parserOptions: {
-        project: ['./jest/tsconfig.json']
-      }
-    }
-  ]
+  plugins: ['react', '@typescript-eslint'],
+  rules: {}
 }
--- components/paper/paper.tsx
+++ components/paper/paper.tsx
@@ -77,14 +77,14 @@ function Paper(): JSX.Element {
       const html = payload?.filter((file) => file.language === 'html')
       const css = payload?.filter((file) => file.language === 'css')
       if (html?.length) {
-        let htmlString: string = ''
+        let htmlString = ''
         html.forEach((htmlFile) => {
           htmlString = htmlString + htmlFile.value
         })
         viewerRef.current.innerHTML = htmlString
       }
       if (css.length) {
-        let cssString: string = ''
+        let cssString = ''
         css.forEach((cssFile) => {
           cssString = cssString + cssFile.value
         })
