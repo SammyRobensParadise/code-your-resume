4109900ab8c42bcde20f9241435fd3dcb88ded06
Sammy Robens-Paradise <srobensparadise@gmail.com>
2022-05-20 23:02:00 -0400

    dev

--- components/editor/editor-container.tsx
+++ components/editor/editor-container.tsx
@@ -6,56 +6,11 @@ import * as files from '../../state/local/files'
 import { File } from '../../types'
 
 export default function EditorContainer() {
-  const mapFiles = files.useFiles()
-
-  useEffect(() => {
-    if (files) {
-      const { defaultFiles } = files
-      defaultFiles.forEach((file: File) => {
-        if (!window.localStorage.getItem(file.id)) {
-          files.updateFileData({
-            operation: 'CREATE',
-            payload: {
-              id: file.id,
-              name: file.name,
-              path: file.name,
-              language: file.language,
-              value: file.value,
-              extension: file.extension
-            }
-          })
-        } else {
-          const data = window.localStorage.getItem(file.id)
-          if (data) {
-            files.updateFileData({
-              operation: 'CREATE',
-              payload: JSON.parse(data)
-            })
-          }
-        }
-      })
-    }
-  }, [])
-
-  function handleOnChange(
-    id: string,
-    value: string | undefined,
-    event: React.ChangeEvent
-  ) {
-    const payload = {
-      id,
-      value: value ? value : '',
-      event
-    }
-    files.updateFileData({
-      operation: 'VALUE',
-      payload
-    })
-  }
+  useEffect(() => {}, [])
 
   return (
     <div className="editors h-full">
-      {mapFiles?.map((file: File) => {
+      {/*?.map((file: File) => {
         const info = file
         return (
           <div key={info.name}>
@@ -65,17 +20,17 @@ export default function EditorContainer() {
                 path={info.name}
                 name={info.name}
                 language={info.language}
-                openEditors={mapFiles.length}
+                openEditors={}
                 theme="vs-dark"
                 defaultValue={info.value}
                 handleOnChange={(value, event) => {
                   handleOnChange(info.id, value, event)
                 }}
               />
-            )}
+              ) }
           </div>
         )
-      })}
+      })*/}
     </div>
   )
 }
---
+++ state/local/store.ts
@@ -0,0 +1,111 @@
+import { UpdateFileDataEvent } from './files'
+import { v4 as uuidv4 } from 'uuid'
+import { File } from '../../types'
+
+export const store = {
+  set: (m: UpdateFileDataEvent): boolean => {
+    switch (m.operation) {
+      case 'CREATE': {
+        const { id, name, path, extension, language, value, isOpen } = m.payload
+        const fallbackExtension = extension ? extension : 'html'
+        const fallbackName = `default${Math.random() * 20}`
+        const newId: string = uuidv4()
+        const newFile: File = {
+          name: name ? name : `${fallbackName}.${fallbackExtension}`,
+          path: path ? path : `${fallbackName}.${fallbackExtension}`,
+          id: id ? id : newId,
+          language: language ? language : 'html',
+          value: value,
+          extension: fallbackExtension,
+          isOpen: isOpen ? isOpen : true
+        }
+
+        // handle local storage
+        const doesExistInLocalSorage = !!(id
+          ? window.localStorage.getItem(id)
+          : null)
+
+        if (!doesExistInLocalSorage) {
+          localStorage.setItem(id ? id : newId, JSON.stringify(newFile))
+        }
+        return true
+      }
+      case 'VALUE': {
+        const { id, value } = m.payload
+        if (id) {
+          const file = window.localStorage.getItem(id)
+          if (file) {
+            const c: File = JSON.parse(file)
+            c.value = value
+            window.localStorage.setItem(id, JSON.stringify(c))
+            return true
+          }
+        }
+        return false
+      }
+
+      case 'NAME': {
+        const { id, name } = m.payload
+        if (id) {
+          const file = window.localStorage.getItem(id)
+          if (file) {
+            const c: File = JSON.parse(file)
+            c.name = name ? name : 'unknown.html'
+            window.localStorage.setItem(id, JSON.stringify(c))
+            return true
+          }
+        }
+        return false
+      }
+      case 'LANGUAGE': {
+        const { id, language } = m.payload
+        if (id) {
+          const file = window.localStorage.getItem(id)
+          if (file) {
+            const c: File = JSON.parse(file)
+            c.language = language ? language : 'html'
+            window.localStorage.setItem(id, JSON.stringify(c))
+            return true
+          }
+        }
+        return false
+      }
+      case 'CLOSE': {
+        const { id } = m.payload
+        if (id) {
+          const file = window.localStorage.getItem(id)
+          if (file) {
+            const c: File = JSON.parse(file)
+            c.isOpen = false
+            window.localStorage.setItem(id, JSON.stringify(c))
+            return true
+          }
+        }
+        return false
+      }
+      case 'OPEN': {
+        const { id } = m.payload
+        if (id) {
+          const file = window.localStorage.getItem(id)
+          if (file) {
+            const c: File = JSON.parse(file)
+            c.isOpen = true
+            window.localStorage.setItem(id, JSON.stringify(c))
+            return true
+          }
+        }
+        return false
+      }
+      default: {
+        return true
+      }
+    }
+  },
+  get: (id: string): File | null => {
+    if (id) {
+      const file = window.localStorage.getItem(id)
+      return file ? JSON.parse(file) : null
+    }
+    return null
+  }
+}
